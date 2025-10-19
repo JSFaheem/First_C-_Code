e437eff3f69a33922c7e97a3de56be2a4c26f644
JSFaheem <fhmchanna@gmail.com>
2025-10-19 15:06:29 +0500

    Comment and Variable

---
+++ tut3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//This program was created by Faheem
+/*this
+is
+a
+multiline*/
+int main(){
+    int sum = 6;
+    cout<< "hello world"<< sum;
+    return 0;
+}
